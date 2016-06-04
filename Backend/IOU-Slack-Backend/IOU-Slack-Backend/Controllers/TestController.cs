0954a02c464a4f4cfe92d7bcb64f0022d9a497db
FelixLeChat <felixlrc21@gmail.com>
2016-06-04 13:06:01 -0400

    Add Test Endpoint

---
+++ Backend/IOU-Slack-Backend/IOU-Slack-Backend/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using IOU_Slack_Backend.Context;
+
+namespace IOU_Slack_Backend.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return "OK :D!";
+        }
+    }
+}
