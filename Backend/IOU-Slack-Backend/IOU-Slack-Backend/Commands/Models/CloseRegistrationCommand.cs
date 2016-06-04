66693bd9a32953c88b179729564f335226d96d7b
Sebastien B <sebastienbernard93@gmail.com>
2016-06-04 18:51:22 -0400

    close registrationcommand added

---
+++ Backend/IOU-Slack-Backend/IOU-Slack-Backend/Commands/Models/CloseRegistrationCommand.cs
@@ -0,0 +1,30 @@
+﻿using IOU_Slack_Backend.Dtos;
+using IOU_Slack_Backend.Helper;
+using IOU_Slack_Backend.Services;
+
+namespace IOU_Slack_Backend.Commands.Models
+{
+    public class CloseRegistrationEvent : Command
+    {
+        public CloseRegistrationEvent(string parameters, CommandRequest commandRequest) : base(parameters, commandRequest)
+        {
+            this.Type = CommandType.SubscribeToEvent;
+        }
+
+        public override void Execute()
+        {
+            var endpoint = "https://ioubot.herokuapp.com/api/close-registration";
+
+            var eventService = new EventService();
+
+            var eventId = eventService
+                .getEventByAdminId(this.Parameters, CommandRequest.User_ID).Id;
+
+            string e = "{ userId:"+CommandRequest.User_ID+"userName:"+CommandRequest.User_Name+"eventId:"+eventId+"}";
+
+            HttpRequestHelper.PostObjectAsync(endpoint, e);
+
+            eventService.CloseEvent(eventId.ToString());
+        }
+    }
+}
