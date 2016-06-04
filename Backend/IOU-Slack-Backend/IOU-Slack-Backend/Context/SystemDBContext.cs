e918c897dc2626805154ceff570dd310d720a2e5
FelixLeChat <felixlrc21@gmail.com>
2016-06-04 17:32:35 -0400

    Remove unused dbset in context

--- Backend/IOU-Slack-Backend/IOU-Slack-Backend/Context/SystemDBContext.cs
+++ Backend/IOU-Slack-Backend/IOU-Slack-Backend/Context/SystemDBContext.cs
@@ -7,9 +7,6 @@ namespace IOU_Slack_Backend.Context
     {
         #region Fields
 
-        public virtual DbSet<User> Users { get; set; }
-        public virtual DbSet<Group> Groups { get; set; }
-        public virtual DbSet<UserInGroup> UsersInGroups { get; set; }
         public virtual DbSet<Transaction> Transactions { get; set; }
         public virtual DbSet<Event> Events { get; set; }
 
