2dcc3f830f2c8d1217218d98191cae3c3911a167
FelixLeChat <felixlrc21@gmail.com>
2016-06-04 12:55:24 -0400

    Add Backend models

---
+++ Backend/IOU-Slack-Backend/IOU-Slack-Backend/Backend-Models/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace IOU_Slack_Backend
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Groupname { get; set; }
+        public string GroupId { get; set; }
+        public string AdminUserId { get; set; }
+    }
+}
