e9b4004016b9ebb228a12d5ff2e6d28b6c0bac68
FelixLeChat <felixlrc21@gmail.com>
2016-06-04 12:31:19 -0400

    ADD ALL

---
+++ Backend/IOU-Slack-Backend/IOU-Slack-Backend/Backend-Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace IOU_Slack_Backend
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string UserId { get; set; }
+    }
+}
