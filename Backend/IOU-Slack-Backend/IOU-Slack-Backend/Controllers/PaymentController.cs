aee54f3f6b4133c5c4699196f9b849aef3f77023
Sebastien Crevier <sebastien.crevier@hotmail.com>
2016-06-05 00:52:34 -0400

    payment controller and payforcommand

---
+++ Backend/IOU-Slack-Backend/IOU-Slack-Backend/Controllers/PaymentController.cs
@@ -0,0 +1,18 @@
+﻿using IOU_Slack_Backend.Services;
+using System.Web.Http;
+
+namespace IOU_Slack_Backend.Controllers
+{
+    [RoutePrefix("api/payment")]
+    public class PaymentController : ApiController
+    {
+        [HttpPost]
+        [Route("{eventId}/{userId}")]
+        public IHttpActionResult ConfirmPayment(int eventId, string userId, double amount)
+        {
+            new DebtService().ReimburseDebt(eventId, userId, amount);
+
+            return this.Ok();
+        }
+    }
+}
